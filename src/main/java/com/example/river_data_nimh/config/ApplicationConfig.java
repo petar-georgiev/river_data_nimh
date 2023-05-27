e78b4f190e8034d585aa8d140588ddcb47f74127
petar-georgiev <georgiev.peter1@gmail.com>
2023-05-27 16:42:14 +0300

    adding actuator

--- src/main/java/com/example/river_data_nimh/config/ApplicationConfig.java
+++ src/main/java/com/example/river_data_nimh/config/ApplicationConfig.java
@@ -1,10 +1,20 @@
 package com.example.river_data_nimh.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Component
-public class ApplicationConfig {
+@Configuration
+public class ApplicationConfig implements WebMvcConfigurer {
     @Value("${host.url}")
     private String hostUrl;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/actuator/**")
+                .allowedOrigins(hostUrl)
+                .allowedMethods("GET");
+    }
 }
