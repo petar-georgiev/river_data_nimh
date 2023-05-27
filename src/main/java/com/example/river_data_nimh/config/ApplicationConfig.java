baf8a452af4fcf1b6f496b911b6515c2e845a747
petar-georgiev <georgiev.peter1@gmail.com>
2023-05-27 16:58:19 +0300

    update

--- src/main/java/com/example/river_data_nimh/config/ApplicationConfig.java
+++ src/main/java/com/example/river_data_nimh/config/ApplicationConfig.java
@@ -3,6 +3,7 @@ package com.example.river_data_nimh.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,6 +13,7 @@ public class ApplicationConfig implements WebMvcConfigurer {
     private String hostUrl;
 
     @Override
+    @CrossOrigin("${host.url}")
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/actuator/**")
                 .allowedOrigins(hostUrl)
